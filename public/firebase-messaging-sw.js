ffc8892a2c93d4ce53181a1177004199a4da1204
Ajmalnk1 <ai.codomail@gmail.com>
2025-05-20 23:17:58 +0530

    bug editing

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -16,6 +16,6 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function(payload) {
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: 'frontend/public/favicon.ico'
+    icon: '/favicon.ico'
   });
 });
