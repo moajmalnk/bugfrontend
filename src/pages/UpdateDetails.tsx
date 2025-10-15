05334be46b0015e85716839d85f1990c319229cb
moajmalnk <moajmalnk@gmail.com>
2025-10-16 04:26:12 +0530

    updated

--- src/pages/UpdateDetails.tsx
+++ src/pages/UpdateDetails.tsx
@@ -278,7 +278,7 @@ const UpdateDetails = () => {
   const getTypeBadgeStyle = (type: string) => {
     switch (type) {
       case "feature": return "bg-blue-100 text-blue-800 border-blue-200";
-      case "fix": return "bg-green-100 text-green-800 border-green-200";
+      case "updation": return "bg-green-100 text-green-800 border-green-200";
       case "maintenance": return "bg-yellow-100 text-yellow-800 border-yellow-200";
       default: return "bg-gray-100 text-gray-800 border-gray-200";
     }
